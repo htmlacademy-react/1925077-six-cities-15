3502492c59c05814f5740dfe18aa805238239212
Валерий Галеев <71322091+webAmoeba@users.noreply.github.com>
2024-02-21 15:32:31 +0400

    feat: добавляет пути для /favorites & /login

--- src/components/app/app.tsx
+++ src/components/app/app.tsx
@@ -3,6 +3,8 @@ import {AppRoute} from '../../types/routes';
 import {PageMain} from '../../pages/page-main/page-main';
 import {PAGEMAIN} from '../../mock/common-mock';
 import {Page404} from '../../pages/page-404/page-404';
+import {PageFavorites} from '../../pages/page-favorites/page-favorites';
+import {PageLogin} from '../../pages/page-login/page-login';
 
 function App() {
   return (
@@ -11,6 +13,14 @@ function App() {
         <Route
           path={AppRoute.Main}
           element={<PageMain placesCount={PAGEMAIN.placesCount} />}
+        />
+        <Route
+          path={AppRoute.Favorites}
+          element={<PageFavorites />}
+        />
+        <Route
+          path={AppRoute.Login}
+          element={<PageLogin />}
         />
         <Route
           path={AppRoute.NotFound}
