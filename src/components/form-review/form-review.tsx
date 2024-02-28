661508d04b363d39f959fc88b91465198cf5b272
Валерий Галеев <71322091+webAmoeba@users.noreply.github.com>
2024-02-28 18:54:01 +0400

    feat: сохраняет review в state

--- src/components/form-review/form-review.tsx
+++ src/components/form-review/form-review.tsx
@@ -1,6 +1,7 @@
-
+import React from 'react';
 
 export function FormReview() {
+  const [review, setReview] = React.useState('');
 
   return (
     <form className="reviews__form form" action="#" method="post">
@@ -41,7 +42,14 @@ export function FormReview() {
           </svg>
         </label>
       </div>
-      <textarea className="reviews__textarea form__textarea" id="review" name="review" placeholder="Tell how was your stay, what you like and what can be improved"></textarea>
+      <textarea
+        className="reviews__textarea form__textarea"
+        id="review" name="review"
+        placeholder="Tell how was your stay, what you like and what can be improved"
+        value={review}
+        onChange={(evt) => setReview(evt.target.value)}
+      >
+      </textarea>
       <div className="reviews__button-wrapper">
         <p className="reviews__help">
         To submit review please make sure to set <span className="reviews__star">rating</span> and describe your stay with at least <b className="reviews__text-amount">50 characters</b>.
