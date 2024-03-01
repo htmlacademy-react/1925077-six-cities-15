1bb74d7103ca176df730267c82716ea45caee278
Валерий Галеев <71322091+webAmoeba@users.noreply.github.com>
2024-03-01 22:46:36 +0400

    feat: создает комп. PlacesSorting

--- src/pages/page-main/page-main.tsx
+++ src/pages/page-main/page-main.tsx
@@ -1,4 +1,5 @@
 import {PlaceCard} from '../../components/blocks/place-card/place-card';
+import {PlacesSorting} from '../../components/blocks/places-sorting/places-sorting';
 import {Tabs} from '../../components/blocks/tabs/tabs';
 import {OFFERS} from '../../mock/offers';
 import {PageMainProps} from '../../types/common-types';
@@ -13,21 +14,7 @@ function PageMain({placesCount}: PageMainProps) {
           <section className="cities__places places">
             <h2 className="visually-hidden">Places</h2>
             <b className="places__found">{placesCount} places to stay in Amsterdam</b>
-            <form className="places__sorting" action="#" method="get">
-              <span className="places__sorting-caption">Sort by</span>
-              <span className="places__sorting-type" tabIndex={0}>
-                Popular
-                <svg className="places__sorting-arrow" width="7" height="4">
-                  <use xlinkHref="#icon-arrow-select"></use>
-                </svg>
-              </span>
-              <ul className="places__options places__options--custom places__options--opened">
-                <li className="places__option places__option--active" tabIndex={0}>Popular</li>
-                <li className="places__option" tabIndex={0}>Price: low to high</li>
-                <li className="places__option" tabIndex={0}>Price: high to low</li>
-                <li className="places__option" tabIndex={0}>Top rated first</li>
-              </ul>
-            </form>
+            <PlacesSorting/>
             <div className="cities__places-list places__list tabs__content">
               {OFFERS.map((offer) => <PlaceCard key={offer.id} {...offer} />)}
             </div>
