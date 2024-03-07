e19d1c26c5b89cbfd06913d100046f467de90859
Валерий Галеев <71322091+webAmoeba@users.noreply.github.com>
2024-03-07 08:45:35 +0400

    fix: правит комп. Tabs

--- src/components/blocks/tabs/tabs.tsx
+++ src/components/blocks/tabs/tabs.tsx
@@ -1,32 +1,47 @@
+import {useState} from 'react';
 import classNames from 'classnames';
 import {NAMES_OF_CITIES} from '../../../mock/common-mock';
 import {CityName} from '../../../types/common-types';
+import {Link} from 'react-router-dom';
 
-export type TabProps = {
+type TabProps = {
   city: CityName;
+  isActive: boolean;
+  onClick: () => void;
 };
 
 export function Tab(props: TabProps) {
   const className = classNames('locations__item-link tabs__item', {
-    // 'tabs__item--active': props.city === isActive,
+    'tabs__item--active': props.isActive,
   });
 
   return (
     <li className="locations__item">
-      <a className={className} href="#">
+      <Link to='/' className={className} onClick={props.onClick}>
         <span>{props.city}</span>
-      </a>
+      </Link>
     </li>
   );
 }
 
 export function Tabs() {
+  const [activeTab, setActiveTab] = useState<string | null>(null);
+
+  const handleTabClick = (city: string) => {
+    setActiveTab(city === activeTab ? null : city);
+  };
+
   return (
     <div className="tabs">
       <section className="locations container">
         <ul className="locations__list tabs__list">
           {NAMES_OF_CITIES.map((city) => (
-            <Tab key={city} city={city}/>
+            <Tab
+              key={city}
+              city={city}
+              isActive={city === activeTab}
+              onClick={() => handleTabClick(city)}
+            />
           ))}
         </ul>
       </section>
