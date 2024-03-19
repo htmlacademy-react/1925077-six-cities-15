18f68a14f69e620491229fc8a67f27f1c8b0e885
Валерий Галеев <71322091+webAmoeba@users.noreply.github.com>
2024-03-19 09:47:28 +0400

    del: удаляет HelmetProvider

--- src/components/global/app/app.tsx
+++ src/components/global/app/app.tsx
@@ -5,7 +5,6 @@ import {Page404} from '../../../pages/page-404/page-404';
 import {PageFavorites} from '../../../pages/page-favorites/page-favorites';
 import {PageLogin} from '../../../pages/page-login/page-login';
 import {PrivateRoute} from '../private-route/private-route';
-import {HelmetProvider} from 'react-helmet-async';
 import {Layout} from '../layout/layout';
 import {PageOffer} from '../../../pages/page-offer/page-offer';
 
@@ -13,42 +12,40 @@ function App() {
   const authorizationStatus = getAuthorizationStatus();
 
   return (
-    <HelmetProvider>
-      <BrowserRouter>
-        <Routes>
-          <Route path={AppRoute.Main} element={<Layout/>}>
-            <Route
-              index
-              element={<PageMain />}
-            />
-            <Route
-              path={AppRoute.Favorites}
-              element={
-                <PrivateRoute authorizationStatus={authorizationStatus}>
-                  <PageFavorites />
-                </PrivateRoute>
-              }
-            />
-            <Route
-              path={AppRoute.Login}
-              element={
-                <PrivateRoute authorizationStatus={authorizationStatus} isReverse>
-                  <PageLogin />
-                </PrivateRoute>
-              }
-            />
-            <Route
-              path={AppRoute.Offer}
-              element={<PageOffer />}
-            />
-          </Route>
+    <BrowserRouter>
+      <Routes>
+        <Route path={AppRoute.Main} element={<Layout/>}>
           <Route
-            path={AppRoute.NotFound}
-            element={<Page404 />}
+            index
+            element={<PageMain />}
           />
-        </Routes>
-      </BrowserRouter>
-    </HelmetProvider>
+          <Route
+            path={AppRoute.Favorites}
+            element={
+              <PrivateRoute authorizationStatus={authorizationStatus}>
+                <PageFavorites />
+              </PrivateRoute>
+            }
+          />
+          <Route
+            path={AppRoute.Login}
+            element={
+              <PrivateRoute authorizationStatus={authorizationStatus} isReverse>
+                <PageLogin />
+              </PrivateRoute>
+            }
+          />
+          <Route
+            path={AppRoute.Offer}
+            element={<PageOffer />}
+          />
+        </Route>
+        <Route
+          path={AppRoute.NotFound}
+          element={<Page404 />}
+        />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
