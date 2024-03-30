e14c03e7fe7aeb07594fd11996c3f9beb13c723c
Валерий Галеев <71322091+webAmoeba@users.noreply.github.com>
2024-03-30 13:32:49 +0400

    fix: добавляет псевдоним типу

--- src/components/blocks/reviews/reviews.tsx
+++ src/components/blocks/reviews/reviews.tsx
@@ -1,9 +1,9 @@
-import {Review} from '../../../types/common-types';
+import {Review as ReviewType} from '../../../types/common-types';
 import {FormReview} from '../form-review/form-review';
 import {Review} from '../review/review';
 
 
-export function Reviews({reviews}: {reviews: Review[]}) {
+export function Reviews({reviews}: {reviews: ReviewType[]}) {
   return (
     <section className="offer__reviews reviews">
       <h2 className="reviews__title">Reviews &middot; <span className="reviews__amount">{reviews.length}</span></h2>
